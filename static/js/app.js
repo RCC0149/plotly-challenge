36709f31eea056c732662efff519293dbad03723
RCC0149 <75042560+RCC0149@users.noreply.github.com>
2021-04-29 21:13:47 -0400

    Completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,51 @@
 // Create fucntion to build plots 
 function buildPlots (id) {
-    d3.json("samples.json").then((data) => {
-        console.log(data)
-        var ids = data.samples[0].otu_ids
-        var sampleValues = data.samples[0].sample_values.slice(0,10).reverse()
-        var labels = data.samples[0].otu_labels.slice(0,10)
-
-        var otu = (data.samples[0].otu_ids.slice(0,10)).reverse()
-        var otuId = otu.map(d => "OTU" + d)
-            console.log(`OTU_labels: ${otuId}`)
-        var labels = data.samples[0].otu_labels.slice(0,10)
-            console.log(`OTU_labels: ${labels}`)
+    d3.json("../../samples.json").then((data) => {
+        // console.log(data)
+        var samples = data.samples
+        var data1 = samples.filter(meta => meta.id.toString() === id)
+        data1 = data1[0]
+        console.log(data1)
+        var ids = data1.otu_ids.slice(0,10)
+        var values = data1.sample_values.slice(0,10)
+        var labels = data1.otu_labels.slice(0,10)
 
 // Create horizontal bar chart
         var trace1 = {
-            x: sampleValues,
-            y: otuId,
+            x: values.reverse(),
+            y: ids.reverse(),
             type: "bar",
             orientation: "h",
-            marker: {
-                color: "blue"
-            },
-            text: labels
+            marker: {color: "blue"},
+            text: labels.reverse()
         }
         var plot = [trace1]
 
         var layout1 = {
             title: "Top 10 OTUs",
-            yaxis: {
-                tickmode: "linear"
-            },
-            margin: {
-                l: 100,
-                r: 100,
-                t: 100,
-                b: 30
-            }
+            xaxis: {title: "Sample Values"},
+            yaxis: {title: "OTU IDs"},
+            height: 700,
+            width: 800
         }
 
     Plotly.newPlot("bar", plot, layout1)
 
 // Create bubble graph
         var trace2 = {
-            x: data.samples[0].otu_ids,
-            y: data.samples[0].sample_values,
+            x: data1.otu_ids,
+            y: data1.sample_values,
             mode: "markers",
             marker: {
-                size: data.samples[0].sample_values,
-                color: data.samples[0].otu_ids
+                size: data1.sample_values,
+                color: data1.otu_ids
             },
-            text: data.samples[0].otu_labels
+            text: data1.otu_labels
         }
 
         var layout2 = {
-            xaxis:{title: "OTU ID"},
+            xaxis:{title: "OTU IDs"},
+            yaxis:{title: "Sample Values"},
             height: 600,
             width: 1000
         }
@@ -67,7 +59,7 @@ function buildPlots (id) {
 
 // Create a function to grab Demo Info
 function getDemoInfo(id) {
-    d3.json("samples.json"). then ((data) => {
+    d3.json("../../samples.json"). then ((data) => {
         var metadata = data.metadata
         console.log(metadata)
 
@@ -92,7 +84,7 @@ function optionChanged(id) {
 function init() {
     var drop = d3.select("#selDataset")
 
-    d3.json("samples.json"). then ((data) => {
+    d3.json("../../samples.json"). then ((data) => {
         data.names.forEach(function(name) {
             drop.append("option").text(name).property("value")
         })
